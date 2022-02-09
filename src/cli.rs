13919ffc189da75a06d435ab61e064f7f3a9dcdf
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-02-09 22:19:09 +0200

    feat: added commit utilities

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,79 @@
-fn new_commit() {
-    let options = vec!["feat", "fix"];
+use dialoguer::{theme::ColorfulTheme, Input, Select};
 
-    //TODO: Reference from here https://www.conventionalcommits.org/en/v1.0.0/#summary
+pub fn new_commit() -> String {
+    let comm_type = commit_type();
+    let comm_scope = commit_scope();
+    let comm_desc = commit_description();
+    let comm_body = commit_body();
 
-    // type
-    // score
-    // description
-    // body
-    // footer
+    let mut commit = format!("{comm_type}");
+
+    if comm_scope.len() > 2 {
+        commit.push_str(&format!("({comm_scope})"))
+    }
+    commit.push_str(&format!(": {comm_desc}"));
+
+    if comm_body.len() > 0 {
+        commit.push_str(&format!("\n\n{comm_body}"));
+    }
+
+    commit
+}
+
+fn commit_type() -> &'static str {
+    let options = vec![
+        "feat", "fix", "docs", "style", "perf", "refactor", "revert", "style", "test", "chore",
+    ];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select type of commit")
+        .default(0)
+        .items(&options)
+        .interact_opt()
+        .unwrap();
+
+    if let Some(selected_opt) = selection {
+        return options[selected_opt];
+    }
+    options[options.len() - 1]
+}
+
+fn commit_scope() -> String {
+    let input: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("scope")
+        .allow_empty(true)
+        .interact_text()
+        .unwrap();
+
+    input
+}
+
+fn commit_description() -> String {
+    let input: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("description")
+        .allow_empty(false)
+        .interact_text()
+        .unwrap();
+
+    input
+}
+
+fn commit_body() -> String {
+    let input: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("body")
+        .allow_empty(true)
+        .interact_text()
+        .unwrap();
+
+    input
+}
+
+fn _commit_footer() -> String {
+    let input: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("footer")
+        .allow_empty(true)
+        .interact_text()
+        .unwrap();
+
+    input
 }
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,30 @@ use clap::{App, AppSettings};
 mod cli;
 
 fn main() {
-    let matches = App::new("rust-cli-demo")
+    // check if the current project is a git project
+    // if it's not info theme
+    // if it's a git project, check the status of the code
+    // if it's clean, run the program
+
+    let matches = App::new("gcm")
         .version("0.1.0")
         .author("Makuza Mugabo Verite")
         .about("Git commits utility")
         .subcommand(
-            App::new("m")
-                .about("Commit all local changes")
+            App::new("b")
+                .about("Breaking changes")
                 .setting(AppSettings::ArgRequiredElseHelp),
         )
         .get_matches();
 
     match matches.subcommand() {
-        Some(("m", sub_matches)) => {
-            println!(
-                "do some git stuffs {}",
-                sub_matches.value_of("msg").unwrap()
-            );
+        Some(("b", _sub_matches)) => {
+            println!("TODO: This is still a work in progress",);
         }
         _ => {
-            println!("gcm was done")
+            let commit_message = cli::new_commit();
+
+            println!("{}", commit_message);
         }
     }
 }
